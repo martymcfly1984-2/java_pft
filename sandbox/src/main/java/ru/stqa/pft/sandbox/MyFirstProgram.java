2361a4dd488cb8d55d9697f444090b53e083b8eb
martymcfly1984-2 <141875652+martymcfly1984-2@users.noreply.github.com>
2023-08-10 23:08:04 +0300

    Variable и типы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main (String [] args){
-        System.out.println("Hello, world");
+    public static void main(String[] args) {
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        int l = 4;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + "=" + s);
+
+        double ll = 4.0;
+        double ss = ll * ll;
+        System.out.println("Площадь квадрата со стороной " + ll + "=" + ss);
     }
 }
